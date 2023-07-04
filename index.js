f0da3a3bafc9e997532a1930add28586011b9b07
Akash Pal <91068643+akashjpal@users.noreply.github.com>
2023-07-04 18:45:45 +0000

    added login and signup functionality

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3001
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const USERS = [];
 
@@ -18,21 +21,67 @@ const SUBMISSION = [
 
 ]
 
+app.get('/signup',function(req,res){
+  res.sendFile(__dirname+"/public/signup.html");
+})
 app.post('/signup', function(req, res) {
   // Add logic to decode body
   // body should have email and password
+  var username = req.body.username;
+  var password = req.body.password;
+  console.log(username+" "+password);
 
-
+  const exist = USERS.filter((username) => username === username);
+  if(exist.length > 0){
+    console.log("User already exists");
+    res.write("User already exists");
+    res.send();
+    return;
+  }
+  else{
+    USERS.push({username:username,password:password});
+  }
+  console.log(USERS);
+ 
+  if(res.statusCode === 200){
+    console.log("User created successfully"+res.statusCode);
+    console.log(res.statusCode);
+    res.redirect("/login");
+  }
   //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
+})
 
-
-  // return back 200 status code to the client
-  res.send('Hello World!')
+app.get('/login',function(req,res){
+  res.sendFile(__dirname+"/public/login.html");
 })
 
 app.post('/login', function(req, res) {
   // Add logic to decode body
   // body should have email and password
+  var username = req.body.username;
+  var password = req.body.password;
+  var confirm_password = req.body.password2;
+
+  const user = USERS.filter((user) => user.username === username);
+  if(user.length === 0){
+    console.log("User does not exist");
+    res.write("User does not exist");
+    res.status(401).send();
+    return;
+  }
+  console.log(password+" "+confirm_password)
+  if(password !== confirm_password){
+    console.log("Password does not match");
+    res.write("Password does not match");
+    res.status(200).send();
+    return;
+  }
+
+  const token =   Math.random().toString(36).substring(2, 15);
+  // res.write(token);
+  // res.write("Login successful");
+  // res.end();
+  res.status(200).redirect("/"+token); // to redirect with dynamic content use this type of route
 
   // Check if the user with the given email exists in the USERS array
   // Also ensure that the password is the same
@@ -41,10 +90,16 @@ app.post('/login', function(req, res) {
   // If the password is the same, return back 200 status code to the client
   // Also send back a token (any random string will do for now)
   // If the password is not the same, return back 401 status code to the client
-
-
-  res.send('Hello World from route 2!')
 })
+
+
+app.get('/:token', function(req, res) {
+  console.log(req.params.token);
+  res.write("Akash is Cool!");
+  res.write("Your token is "+req.params.token);
+  res.send();
+})
+
 
 app.get('/questions', function(req, res) {
 
